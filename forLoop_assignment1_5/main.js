fac7f4bfb91fe9eb8afbf2bf4a146441adf34c9e
HoorainNasir <hoorain.nasir001@gmail.com>
2024-05-23 21:11:45 +0500

    for_loop assignment

---
+++ forLoop_assignment1_5/main.js
@@ -0,0 +1,53 @@
+//1. Identify and print all even numbers in an array of integers
+let integers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let evenNumber of integers) {
+    if (evenNumber % 2 === 0)
+        console.log(evenNumber);
+}
+console.log("These all are even numbers.");
+console.log("====================");
+//2. Find the sum of the first 10 numbers
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+console.log(`the sum of first 10 numbers is: ${sum}`);
+console.log("====================");
+//3. Print the multiplication table for the number 5
+console.log('here i print the table of 5.');
+let number = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} X ${i} = ${number * i}`);
+}
+console.log('======================');
+//4. Print the multiplication table for any number provided by the user
+console.log('provide any number to print the table.');
+import inquirer from "inquirer";
+let num1 = await inquirer.prompt({
+    name: "number",
+    message: "Enter your number.",
+    type: "number"
+});
+let num2 = num1.number;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num2} X ${i} = ${num2 * i}`);
+}
+console.log('===================');
+//5. Print the multiplication table for a number provided by the user,
+//and also take the end of the table as input from the user
+console.log('give any number to print the table & also give the end point of table.');
+let num3 = await inquirer.prompt({
+    name: "number",
+    message: "Enter your number.",
+    type: "number"
+});
+let endPoint = await inquirer.prompt({
+    name: "number",
+    message: "Enter the end point of table.",
+    type: "number"
+});
+let digit = num3.number;
+let end = endPoint.number;
+for (let i = 1; i <= end; i++) {
+    console.log(`${digit} X ${i} = ${digit * i}`);
+}
---
+++ forLoop_assignment6_9/main.ts
@@ -0,0 +1,45 @@
+//6. Print the elements of an array in reverse
+let array=[1, 2, 3, 4, 5]
+
+for(let i= array.length-1; i>=0; i--) {
+    console.log(array[i])
+}
+console.log("==============")
+
+//7. Find the largest number in an array
+let number =[10, 20, 30, 40, 50, 100]
+let largest= number[0]
+
+for(let i=1; i<number.length; i++) {
+    if(number[i]> largest) {
+        largest =number[i]
+    }
+}
+    console.log(`Largest number of array is: ${largest}`)
+    console.log('================')
+
+//8. Find the shortest number in an array
+let short= number[0]
+
+for(let i=1; i<number.length; i++) {
+    if(number[i]< short) {
+        short =number[i]
+    }
+}
+    console.log(`shortest number of array is: ${short}`)
+    console.log("================")
+
+//9. Find prime numbers within a range of 1 to 100
+for(let num= 2; num<= 100; num++){
+    let prime:boolean= true;
+    for(let i =2; i <=Math.sqrt(num); i++){
+        if (num % i === 0) {
+            prime = false;
+            break;
+        }
+}
+if (prime) {
+    console.log(num);
+}
+}
+console.log('They all are prime numbers under 100.')
